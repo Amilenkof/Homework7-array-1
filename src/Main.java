a00aa506eaa5e9d8d4696f74bdc8da10aedf55c2
Amilenkof <f.o.g@list.ru>
2023-02-13 21:09:40 +0400

    homework is done - with tasks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
         task1();
-
+        task2();
+        task3();
+        task4();
     }
 
     public static void task1() {
@@ -21,9 +24,17 @@ public class Main {
  количество данных определите сами. Самостоятельно выберите способ создания массива:
  с помощью ключевого слова или сразу заполненный элементами.*/
         int[] thirstArray = {10, 20, 38, 3214, 5411, 996, 4127, 8};
+    }
+
+    public static void task2() {
+        int[] firstArray = new int[3];
+        firstArray[0] = 1;
+        firstArray[1] = 2;
+        firstArray[2] = 3;
+        double[] secondArray = {1.57, 7.654, 9.986};
+        int[] thirstArray = {10, 20, 38, 3214, 5411, 996, 4127, 8};
+
         System.out.println("Задача 2.");
-        /*Распечатайте на отдельной строчке элементы каждого массива по порядку через запятую. В конце строки запятую
-        ставить не надо.*/
         for (int i = 0; i < firstArray.length; i++) {
             System.out.print(firstArray[i] + " ");
         }
@@ -39,8 +50,16 @@ public class Main {
         System.out.println();
         /*Распечатайте на отдельной строчке элементы каждого массива в обратном порядке через запятую. В конце строки
          запятую ставить не надо.*/
-        System.out.println("Задача 3.");
+    }
 
+    public static void task3() {
+        System.out.println("Задача 3.");
+        int[] firstArray = new int[3];
+        firstArray[0] = 1;
+        firstArray[1] = 2;
+        firstArray[2] = 3;
+        double[] secondArray = {1.57, 7.654, 9.986};
+        int[] thirstArray = {10, 20, 38, 3214, 5411, 996, 4127, 8};
         for (int i = firstArray.length - 1; i >= 0; i--) {
             if (i == 0) {
                 System.out.print(firstArray[i]);
@@ -65,23 +84,30 @@ public class Main {
                 break;
             }
             System.out.print(thirstArray[i] + ", ");
-    }
+        }
         System.out.println();
-
+    }
 
    /* Задача 4
     Пройдитесь по первому целочисленному массиву и все нечетные числа в нем сделайте четными (нужно прибавить 1).
             Важно: код должен работать с любым целочисленным массивом, поэтому для решения задания используйте циклы.
             Распечатайте результат преобразования в консоль.*/
-    System.out.println("Задача 4.");
-    for (int i = 0; i < firstArray.length; i++) {
-        if (firstArray[i]%2==0) {
+
+    public static void task4() {
+        System.out.println("Задача 4.");
+        int[] firstArray = new int[3];
+        firstArray[0] = 1;
+        firstArray[1] = 2;
+        firstArray[2] = 3;
+        double[] secondArray = {1.57, 7.654, 9.986};
+        int[] thirstArray = {10, 20, 38, 3214, 5411, 996, 4127, 8};
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] % 2 == 0) {
+            } else {
+                firstArray[i] += 1;
+            }
         }
-        else {firstArray[i]+=1;
-        }}
         System.out.println(Arrays.toString(firstArray));
-
-
-}
+    }
 }
 
